dcecb3f975bd416ce71ad0fc2eae18a4413c4c00
duhdumbz <adammukhlis10@yahoo.com>
2025-03-13 22:08:03 +0800

    cloudsssss

--- Games Dev/Assets/_Scripts/CloudsController.cs
+++ Games Dev/Assets/_Scripts/CloudsController.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,20 +5,20 @@ using UnityEngine;
 public class CloudsController : MonoBehaviour
 {
     [SerializeField]
-    private Transform _cloud1;
-    [SerializeField]
     private Transform[] _clouds = new Transform[6];
+    [SerializeField]
+    private float _speed = 1.0f;
     void Start()
     {
-        _clouds[0] = _cloud1;
+
     }
 
     void Update()
     {
-
-
+        for (int i = 0; i < _clouds.Length; i++)
+        {
+            _clouds[i].position 
+                = _clouds[i].position + Vector3.right * Time.deltaTime;
+        }
     }
 }
-=======
-﻿
->>>>>>> 74b8ca90e5d33d171939067fe16b4a0a6cc3b734
