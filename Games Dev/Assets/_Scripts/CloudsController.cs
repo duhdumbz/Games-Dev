e571593e145fb974ad9590e92c26e29227600e04
duhdumbz <adammukhlis10@yahoo.com>
2025-03-13 23:12:10 +0800

    .

--- Games Dev/Assets/_Scripts/CloudsController.cs
+++ Games Dev/Assets/_Scripts/CloudsController.cs
@@ -8,6 +8,8 @@ public class CloudsController : MonoBehaviour
     private Transform[] _clouds = new Transform[6];
     [SerializeField]
     private float _speed = 1.0f;
+    [SerializeField]
+    private float _xLimit = 12.5f;
     void Start()
     {
 
@@ -18,7 +20,15 @@ public class CloudsController : MonoBehaviour
         for (int i = 0; i < _clouds.Length; i++)
         {
             _clouds[i].position 
-                = _clouds[i].position + Vector3.right * Time.deltaTime;
+                = _clouds[i].position + Vector3.right * Time.deltaTime *_speed;
+           
+            if (_clouds[i].position.x > _xLimit)
+            {
+                _clouds[i].position -= new Vector3(2 * _xLimit, 0, 0);
+            }
+
         }
+
+
     }
 }
--- Games Dev/Assets/_Scripts/Weapon.cs
+++ Games Dev/Assets/_Scripts/Weapon.cs
@@ -1,1 +1,19 @@
-﻿
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    // Start is called before the first frame update void Start ()
+    void Start()
+    {
+
+    }
+
+        //Update is called once per frame
+        void Update()
+        {
+            transform.RotateAround(Vector3.zero, Vector3.forward,200 * Time.deltaTime);
+        }
+    }
+
