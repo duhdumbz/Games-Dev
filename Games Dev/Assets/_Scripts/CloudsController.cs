569ed8a3a338c3c3ff3abf278b48052beb99b266
duhdumbz <adammukhlis10@yahoo.com>
2025-03-13 21:49:37 +0800

    .

--- Games Dev/Assets/_Scripts/CloudsController.cs
+++ Games Dev/Assets/_Scripts/CloudsController.cs
@@ -1,1 +1,21 @@
-﻿usin
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudsController : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _cloud1;
+    [SerializeField]
+    private Transform[] _clouds = new Transform[6];
+    void Start()
+    {
+        _clouds[0] = _cloud1;
+    }
+
+    void Update()
+    {
+
+
+    }
+}
